86c0cb179746ad6a8fab9ec8669399d52b53aab1
Andre Ranussi <ranussi.a@gmail.com>
2023-08-24 13:09:51 +0100

    finalised the project

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Project Name
+# Magic 8 Ball Game
+
+This project was a simple method to practice DOM manipulation, HTML, CSS and JS. There was not much challenge to this project apart from figuring out the logic to use, although this did not teach me much, it is a good sign that basic projects are becoming natural to me.
 
 # Hosted
 
--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+const button = document.querySelector("button");
+const display = document.querySelector(".result");
+const input = document.querySelector("#question");
+const answers = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook is good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
 
+const answerGenerator = function () {
+  let randomNumber = Math.floor(Math.random() * answers.length);
+  const result = answers[randomNumber];
+  display.textContent = result;
+  display.style.fontSize = "1rem";
+
+  setTimeout(function () {
+    display.textContent = "8";
+    display.style.fontSize = "5rem";
+    input.value = "";
+  }, 1500);
+
+  console.log(result);
+};
+
+button.addEventListener("click", answerGenerator);
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    answerGenerator();
+  }
+});
